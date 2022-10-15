2a7bdc8743cb7798ee6ac6141edc308d187ebf1d
HenryMuema <wambuamuemaa@gmail.com>
2022-10-15 08:38:24 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int number;
+	if (number>0)
+	{
+		printf("is positive\n");
+	}
+	else if (number=0)
+	{
+		printf("is zero\n");
+	}
+	else 
+	{
+		printf("is negative\n");
+	}	
+	return(0);
+}
