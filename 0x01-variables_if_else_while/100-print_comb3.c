66099ec8605d8373ae70d00fc0f459c30f7ee204
HenryMuema <wambuamuemaa@gmail.com>
2022-10-17 05:58:57 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 
 /*
- * program that prints all possible different combinations of two di*gits.
+ * program that prints all possible different combinations of two
+ * digits
  */
-int main(void)
+int main (void)
 {
 	int a, b;
 
-	for (a = 0; a <= 8; a++)
+	for (a = '0'; a <= '8'; a++)
 	{
-		for(b =1; b > a; b++)
+		for (b ='1'; b <= '9'; b++)
 		{
-			putchar()
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
