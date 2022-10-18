1ebed727afeb2e6e968da57428ff8dc09c9e2166
HenryMuema <wambuamuemaa@gmail.com>
2022-10-18 06:10:37 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - prints a satement
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char strg[] = "Programming is like building a multilingual puzzle\n";
+		puts(strg);
+		return (0);
+}
