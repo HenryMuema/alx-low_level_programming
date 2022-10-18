f2cb184360830ce3c470195a34f3dd02d4c23ad0
HenryMuema <wambuamuemaa@gmail.com>
2022-10-18 15:59:57 -0700

    new task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,14 @@
-#ifndef main.h
-#define main.h
-
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * print_alphabet - prints lowercase alphabets
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	char c;
+void print_alphabet(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_times_table(int n);
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-}
-
-#endif
