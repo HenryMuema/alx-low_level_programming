346d200a985ffb479178294a7c574ead0706cc7c
HenryMuema <wambuamuemaa@gmail.com>
2022-10-18 06:25:09 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - prints a statement
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char e[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", e);
+	return (0);
+}
