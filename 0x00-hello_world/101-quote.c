8b073cbff16990c538664413193acc2af4aadc23
HenryMuema <wambuamuemaa@gmail.com>
2022-10-18 12:58:54 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - prints a statement followed by anew line
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char strg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	printf("%s\n", strg);
+	return (1);
+}
