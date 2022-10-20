9beb500acd3575ed4663c65cc09fd5384f82a46f
HenryMuema <wambuamuemaa@gmail.com>
2022-10-20 06:23:30 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - tests function that prints if integer is positive or negat
+ *ive
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
