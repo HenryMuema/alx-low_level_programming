12d447d7dd7ced0c0ad0c5caf5bf54e31f6fd648
HenryMuema <wambuamuemaa@gmail.com>
2022-10-25 08:54:06 -0700

    files included

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * _puts(char *str) -  prints a string, followed by a new line, to stdout
+ * @str: pointer of character type
+ *
+ * Return: void
+ */
+void _puts(char *str);
+{
+	printf("%s\n", str);
+}
