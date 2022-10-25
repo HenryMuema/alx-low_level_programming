0a634d98ba4e7f0472227337b97b099b2b393098
HenryMuema <wambuamuemaa@gmail.com>
2022-10-25 08:21:39 -0700

    task 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  *reset_to_98 - assigns new value
- *@*n: poinnter of integer type
+ *@n: poinnter of integer type
  *
  * Return: void
  */
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer of character type
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
