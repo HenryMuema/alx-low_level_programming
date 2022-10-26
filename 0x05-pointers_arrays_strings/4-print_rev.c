6f39f2718ace4f00642024613fa49a5185dcb2c8
HenryMuema <wambuamuemaa@gmail.com>
2022-10-26 07:01:56 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<string.h>
+#include<stdio.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s: string pointer to reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char st = strrev(s);
+	puts(st);
+}
