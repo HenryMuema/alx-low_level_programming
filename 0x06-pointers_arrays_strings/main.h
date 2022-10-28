91a31b523de039fa46be680a98d59edc465803c7
HenryMuema <wambuamuemaa@gmail.com>
2022-10-28 14:43:30 +0000

    tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
+void print_number(int n);
 >>>>>>> ab7dfb1cc5ab281d6e0a30e01fccbfa1814aad66
