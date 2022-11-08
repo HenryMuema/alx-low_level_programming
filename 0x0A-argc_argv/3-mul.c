6309bfa78a20fc6cf396da923875dedf927877c8
HenryMuema <wambuamuemaa@gmail.com>
2022-11-08 10:07:26 +0000

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,17 @@ int main(int argc, char *argv[])
 	int mult, a;
 
 	mult = 1;
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
 	for (a = 1; a < argc; a++)
 	{
 		mult *= atoi(argv[a]);
 	}
 	printf("%d\n", mult);
-	if (a == 0)
-	{
-		printf("Error\n");
 	}
 	return (1);
 }
