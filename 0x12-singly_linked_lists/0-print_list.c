455c6f3979f929254157eb91aaa6a89b00288069
HenryMuema <wambuamuemaa@gmail.com>
2022-11-28 13:54:54 +0000

    files only

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *print_list - prints all the elements of a list
+ *@h: pointer to the list
+ *
+ *Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *head, *node1, *current;
+
+	head = current = 0;
+
+	while (current.next != NULL)
+	{
+		 
+	node1 = (list_t *)malloc(sizeof(list_t));
+	head = node1;
+	head.str = str;
+	head.len = len;
+	head.next = 
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+An introduction to single linked lists
