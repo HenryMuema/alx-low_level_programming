f8f12570dee9b2668d4467553149bf0da7f2c6da
HenryMuema <wambuamuemaa@gmail.com>
2022-12-06 13:26:17 +0000

    TASKS

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,12 +8,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int a, b;
+
 	if (index > (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	unsigned int a, b = 1 << index;
-
+	b = 1 << index;
 	a = ~b;
 
 	*n &= a;
